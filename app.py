2733ad83afecfed65a228262b1b80faa5c596c16
Prince Solanki <sol.prince95@gmail.com>
2026-06-16 21:44:23 +0530

    Fix browser auto-open on Linux

--- app.py
+++ app.py
@@ -208,7 +208,8 @@ import webbrowser
 import threading
 
 def open_browser():
-    webbrowser.open("http://127.0.0.1:5000")
+    import subprocess
+    subprocess.Popen(['xdg-open', 'http://127.0.0.1:5000'])
 
 threading.Timer(1.5, open_browser).start()
 app.run(host="127.0.0.1", port=5000)
